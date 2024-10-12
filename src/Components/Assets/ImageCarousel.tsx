d04506a2d29f73536fcdad4d425d62a24d905382
Sujit Maharana <sujit.maharana@vidyutva.com>
2024-10-12 15:12:50 +0530

    Redesign the project roadmap carousel and updates in about us project carousel

--- src/Components/Assets/ImageCarousel.tsx
+++ src/Components/Assets/ImageCarousel.tsx
@@ -6,10 +6,10 @@ const row3 = images.row3;
 
 const ImageCarousel = () => {
     return (
-        <div className="bg-gradient-main relative h-[100vh] md:h-[80vh] sm:h-[70vh] min-[479px]:h-[65vh] min-[350px]:h-[60vh] min-[300px]:h-[55vh]  w-full flex items-center justify-center overflow-hidden">
+        <div className="bg-gradient-main relative h-[100vh] md:h-[70vh] sm:h-[65vh] min-[479px]:h-[55vh] min-[350px]:h-[60vh] min-[300px]:h-[55vh]  w-full flex items-center justify-center overflow-hidden">
             <div className="absolute top-0 left-0 w-full h-14 bg-gradient-top z-10"></div>
             <div className="absolute bottom-0 left-0 w-full h-16 bg-gradient-bottom z-10"></div>
-            <div className="grid grid-cols-3  gap-8 px-8">
+            <div className="grid grid-cols-3 2xl:gap-8 lg:gap-6 md:gap-4 gap-2 2xl:px-8 lg:px-6 md:px-4 px-2">
                 <div className="col-span-1  ">
                     <div className=" animate-scrollY space-y-8">
                         {[...row1].map((el, index) => (
--- src/Components/ProjectRoadmap/ProjectRoadmap.tsx
+++ src/Components/ProjectRoadmap/ProjectRoadmap.tsx
@@ -1,19 +1,68 @@
 'use client'
 import React from 'react'
 import Headings from '../Assets/Headings'
-import RoadmapCard from '../Assets/RoadmapCard'
+import { Card, Carousel } from '../Assets/RoadMap_Card'
+
+const data = [
+    {
+        phase: "Phase 01",
+        percentage: "0%",
+        title: "Let's Start our Journey",
+        description: "Initiate the project by defining objectives, setting goals, and creating a roadmap aligned with strategic goals.",
+        points: [],
+    },
+    {
+        phase: "Phase 02",
+        percentage: "10%",
+        title: "Research ",
+        description: "Analyze market trends and insights to create a design that prioritizes user needs and functionality.",
+        points: ["Strategic Decisions", "Amplify success potential", "Operational Action Plans"]
+    },
+    {
+        phase: "Phase 03",
+        percentage: "35%",
+        title: "Designing",
+        description: "Create wireframes and prototypes to craft an intuitive, engaging user experience and interface..",
+        points: ["Credibility and expertise  ", "Engage target audience", "Cutting-edge design"]
+    },
+    {
+        phase: "Phase 04",
+        percentage: "70%",
+        title: "Development",
+        description: "Develop and code the website, integrating features for optimal performance and seamless usability on all devices.",
+        points: ["Tech-Driven Approach ", "Optimal Performance ", "Quality and Ethical Standard"]
+    },
+    {
+        phase: "Phase 05",
+        percentage: "85%",
+        title: "Testing",
+        description: "Implement comprehensive testing for functionality and usability to ensure seamless operation and superior performance.",
+        points: ["Continuous Monitoring", "Scalability & Error Tracking", "Security Evaluation"]
+    },
+    {
+        phase: "Phase 06",
+        percentage: "100%",
+        title: "Launch",
+        description: "Formulate a dynamic launch plan to generate enthusiasm and appeal to early customers.",
+        points: ["Ignite Growth", "Collect Significant feedback ", "engage potential investor and partners"]
+    }
+];
 
 const ProjectRoadmap = () => {
+
+    const cards = data.map((card, index) => (
+        <Card key={card.phase} card={card} index={index} />
+    ));
     return (
         <>
-            <div className='Roadmap'>
+            <div className='Roadmap py-6 2xl:px-16 lg:px-14 md:px-12 sm:px-10 px-4'>
                 <div className='flex flex-col gap-16'>
                     <Headings
                         heading='Project Roadmap'
                         subHeading='Our Journey'
                         description='Outlined steps to achieve our goals and future vision.'
                     />
-                    <RoadmapCard />
+                    <Carousel items={cards} />
                 </div>
             </div>
         </>
